05bf478dce47e4cc4cee6659802b42fb73b6710d
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-24 22:20:39 +0330

    Create application

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('ok')
+import * as express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+
+app.listen(port , ()=>{
+    console.clear()
+    console.log(process.version)
+    console.log(`app runing on port ` , port)
+})
