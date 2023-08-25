bd88f8eadccd296c3fe1061433c1c9b49494d8ff
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-25 20:48:52 +0330

    Change app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
-import * as express from 'express';
+import "reflect-metadata"
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express'
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+    
+})
+
+
+server.applyMiddleware({app})
 
 
 app.listen(port , ()=>{
