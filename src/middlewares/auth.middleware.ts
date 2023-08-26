89fa849fa62d15fa8e59ff344cd9de3c604176ce
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2023-08-26 23:22:24 +0330

    Add auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,15 @@
+import { AuthChecker } from 'type-graphql';
+import { ContextType } from '../interfaces/context.type'; 
+import passport from 'passport';
+
+export const authChecker: AuthChecker<ContextType> = async ({ context }) => {
+  return new Promise<boolean>((resolve, reject) => {
+    passport.authenticate('jwt', { session: false }, (err, user) => {
+      if (err || !user) {
+        return resolve(false);
+      }
+      context.req.user = user; 
+      resolve(true);
+    })(context.req, context.res);
+  });
+};
